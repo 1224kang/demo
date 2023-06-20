c421fa51fd9af1315daae97f7fc6d5698aa5d455
Kang min seo <cherry1224@sookmyung.ac.kr>
2023-06-20 14:15:10 +0900

    백엔드개발 18.@NameQuery,@Query 사용하기

--- model/TodoEntity.java
+++ model/TodoEntity.java
@@ -4,11 +4,9 @@ package com.example.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
-
 import org.hibernate.annotations.GenericGenerator;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
